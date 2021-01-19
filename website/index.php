01c3c09d521e1a83cd436fa4ffbb0c1e5ae3a2a2
Arjan14 <arjan140200@gmail.com>
2021-01-19 14:00:38 +0100

    Styles.css added

--- website/index.php
+++ website/index.php
@@ -1,4 +1,17 @@
 <html>
-<h1>test</h1>
-<h2>test2</h2>
+<head>
+    <title>Osaka University Dashboard</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="title">
+        <h1>Osaka University Dashboard</h1>
+    </div>
+
+    <div class="sidebar">
+        <button>Download data</button>
+        <button>Logout</button>
+    </div>
+
+</body>
 </html>
