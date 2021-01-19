8688975c76c14e6638325f179d035cc54668529b
Arjan14 <arjan140200@gmail.com>
2021-01-19 14:48:04 +0100

    Basic layout WIP

--- website/index.php
+++ website/index.php
@@ -4,14 +4,26 @@
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <div class="title">
+
+<div class="title">
         <h1>Osaka University Dashboard</h1>
     </div>
 
     <div class="sidebar">
-        <button>Download data</button>
-        <button>Logout</button>
+
+        <div class="image"></div>
+
+        <a href="login.php">Download files</a>
+        <a href="login.php">Logout</a>
+
+
     </div>
 
+    <div class="top10"></div>
+    <div class="weatherinfo"</div>
+
+
+    <div class="graphl"></div>
+    <div class="graphr"></div>
 </body>
 </html>
