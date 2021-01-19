c491435a4e94032daa19c2b0b351d664e64a137f
Ruben <rubeneekhof@gmail.com>
2021-01-19 13:04:07 +0100

    index.html verandert naar index.php

---
+++ website/login.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file login.php
+ *
+ * @brief On this page the admin can login to the website
+ */
+
+// Since there is only 1 user the password is hardcoded into the website.
+$wachtwoord = "leenfiets123";
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+
+	<head>
+	<body>
+
+	</body>
+</html>
