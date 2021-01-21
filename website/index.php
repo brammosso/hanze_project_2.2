a13c45a6f645f271c3dbe42fd04a463b9232ba1e
Ruben <rubeneekhof@gmail.com>
2021-01-21 11:05:44 +0100

    Added a functional login system along with documentation

--- website/index.php
+++ website/index.php
@@ -1,3 +1,11 @@
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+}
+if (!isset($_SESSION["logged_in"])) {
+    header('Location: login.php');
+}
+?>
 <html>
 <head>
     <title>Osaka University Dashboard</title>
--- website/login.php
+++ website/login.php
@@ -5,8 +5,13 @@
  * @brief On this page the admin can login to the website
  */
 
+// Starting a session if one has not yet been established.
+if (!isset($_SESSION)) {
+    session_start();
+}
 // Since there is only 1 user the password is hardcoded into the website.
-$wachtwoord = "leenfiets123";
+$correct_username = "tester";
+$correct_password = "leenfiets123";
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,6 +19,34 @@ $wachtwoord = "leenfiets123";
 
 	<head>
 	<body>
-
+		<form method="post" id="LoginForm">
+		    <input name="username" id="username" placeholder="Username"> <br />
+		    <input type="password" name="password" id="password" placeholder="Password"> <br />
+		    <input type="submit" name="login" value="Login">
+		</form>
 	</body>
 </html>
+<?php
+// Checking if the login form has been submitted.
+if(isset($_POST['login'])){
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	// Checking if all the fields have been filled in.
+	$fields = array('username', 'password');
+	$missing_field = false;
+	foreach ($fields as $fieldname) {
+        if (!isset($_POST[$fieldname]) || empty($_POST[$fieldname])) {
+            echo $fieldname . " has not been filled in.<br />";
+            $error = true;
+        }
+    }
+	// Checking the username and passwords are correct
+	if ($correct_username == $username && $correct_password == $password) {
+		// If the login is succesfull we start a session
+		$_SESSION["logged_in"] = true;
+		header('Location: index.php');
+	} else {
+		echo "Username or password is incorrect.";
+	}
+}
+?>
