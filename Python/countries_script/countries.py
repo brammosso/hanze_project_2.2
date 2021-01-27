ac7d9cd1db093e50842e6e04e63d468c6717658a
brammosso <derpedederpderp@gmail.com>
2021-01-27 14:29:11 +0100

    Vaste grootte station nummers

--- Python/countries_script/countries.py
+++ Python/countries_script/countries.py
@@ -12,14 +12,14 @@ stations = json.loads(station_data.read())
 print(countries)
 
 for station in stations[2]['data']:
-	stn_nr = station['stn']
+	stn_nr = int(station['stn'])
 	stn_name = station['name']
 	stn_country = station['country']
 	stn_latitude = station['latitude']
 	stn_longitude = station['longitude']
 
 	if stn_country in countries:
-		stn_data = f'{stn_nr};{stn_name};{stn_country};{stn_longitude};{stn_latitude}'
+		stn_data = f'{stn_nr:06d};{stn_name};{stn_country};{stn_longitude};{stn_latitude}'
 		stations_europa_japan.write(stn_data + '\n')
 		if stn_country == 'JAPAN':
 			pass
